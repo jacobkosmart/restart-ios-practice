bfd657b48d73e8db1c3665b39e2702b1a53aa20a
jacobkosmart <jacobkosmart@gmail.com>
2022-01-07 06:44:14 +0900

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Jacob Ko on 2022/01/07.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
